b42aa29eed50b63232bbb74fbb0a33620a45b1b0
dzakyatha <18223124@std.stei.itb.ac.id>
2026-01-22 16:39:23 +0700

    feat: Simplify user profile creation by removing unused save_user_profile in signals.py

--- backend/users/signals.py
+++ backend/users/signals.py
@@ -9,11 +9,3 @@ def create_user_profile(sender, instance, created, **kwargs):
     """
     if created:
         Profile.objects.create(user=instance)
-
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    """
-    Save the Profile whenever the User is saved
-    """
-    if hasattr(instance, 'profile'):
-        instance.profile.save()
